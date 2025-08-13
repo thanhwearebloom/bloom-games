ca42646d3032d97993f29fe56efbeb679bbd0f55
Thanh <nguyenphucthanh@live.com>
2025-08-13 15:53:38 +0700

    build: fix

--- app/firebase-admin.ts
+++ app/firebase-admin.ts
@@ -3,9 +3,9 @@ import admin from "firebase-admin";
 
 const adminApp = initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FB_PROJECT_ID,
-    privateKey: process.env.FB_PRIVATE_KEY,
-    clientEmail: process.env.FB_CLIENT_EMAIL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    privateKey: import.meta.env.VITE_FIREBASE_PRIVATE_KEY,
+    clientEmail: import.meta.env.VITE_FIREBASE_CLIENT_EMAIL,
   }),
 });
 
